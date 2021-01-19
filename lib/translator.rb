baca5da67e860d1e811aea34110b0cdb5deaff75
Liz Burke <liz.burke.chi@gmail.com>
2021-01-19 00:21:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,21 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+final_hash = {}
+YAML.load_file(path).each do |key, value|
+  final_hash[key] = {}
+  final_hash[key][:english] = value[0]
+  final_hash[key][:japanese] = value[1]
+  binding.pry
 end
+final_hash
+end
+
+
+
+
 
 def get_japanese_emoticon
   # code goes here
