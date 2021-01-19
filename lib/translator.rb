c78315e6ed07d765efcc0682aed6c7b33ae83d89
Liz Burke <liz.burke.chi@gmail.com>
2021-01-19 00:54:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,9 +25,8 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
-    if value[:english] == emoticon
+    
       return key
-binding.pry
 end
   end
 end
